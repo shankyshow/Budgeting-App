a0c54b0b178fd000e5190a7ef6db8e2adcd85be9
shanky <ysashank@hotmail.com>
2018-02-27 01:43:06 +0100

    Dashboard Layout as per UX V1

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -7,70 +7,115 @@ import * as dashboard from 'dashboard.json';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-  selectDefault = 1;
+  userTab(): number {
+    return (<any>dashboard).defTab;
+  }
 
-  /* Main Balance Summary */
-    getYearBal(): number {
-      return (<any>dashboard).yearBal;
-    }
+/* MAIN BALANCE Summary */
+  getYearBal(): number {
+    return (<any>dashboard).yearBal;
+  }
 
-    getMonthBal(): number {
-      return (<any>dashboard).monthBal;
-    }
+  getMonthBal(): number {
+    return (<any>dashboard).monthBal;
+  }
 
-    getWeekBal(): number {
-      return (<any>dashboard).weekBal;
-    }
+  getWeekBal(): number {
+    return (<any>dashboard).weekBal;
+  }
 
-    getBiWeekBal(): number {
-      return (<any>dashboard).biweekBal;
-    }
+  getBiWeekBal(): number {
+    return (<any>dashboard).biweekBal;
+  }
 
-    getDayBal(): number {
-      return (<any>dashboard).dayBal;
-    }
+  getDayBal(): number {
+    return (<any>dashboard).dayBal;
+  }
 
-  /* Expense Summary */
-    getYearExp(): number {
-      return (<any>dashboard).yearExp;
-    }
+/* EXPENSE SUMMARY Balance */
+  getYearExp(): number {
+    return (<any>dashboard).yearExp;
+  }
 
-    getMonthExp(): number {
-      return (<any>dashboard).monthExp;
-    }
+  getMonthExp(): number {
+    return (<any>dashboard).monthExp;
+  }
 
-    getWeekExp(): number {
-      return (<any>dashboard).weekExp;
-    }
+  getWeekExp(): number {
+    return (<any>dashboard).weekExp;
+  }
 
-    getBiWeekExp(): number {
-      return (<any>dashboard).weekExp;
-    }
+  getBiWeekExp(): number {
+    return (<any>dashboard).biweekExp;
+  }
 
-    getDayExp(): number {
-      return (<any>dashboard).dayExp;
-    }
+  getDayExp(): number {
+    return (<any>dashboard).dayExp;
+  }
 
-  /* Income Summary */
-    getYearInc(): number {
-      return (<any>dashboard).yearInc;
-    }
+/* EXPENSE SUMMARY Target Check */
+  getYearStat(): number {
+    return (<any>dashboard).yearStat;
+  }
 
-    getMonthInc(): number {
-      return (<any>dashboard).monthInc;
-    }
+  getMonthStat(): number {
+    return (<any>dashboard).monthStat;
+  }
 
-    getWeekInc(): number {
-      return (<any>dashboard).weekInc;
-    }
+  getWeekStat(): number {
+    return (<any>dashboard).weekStat;
+  }
 
-    getBiWeekInc(): number {
-      return (<any>dashboard).weekInc;
-    }
+  getBiWeekStat(): number {
+    return (<any>dashboard).biweekStat;
+  }
 
-    getDayInc(): number {
-      return (<any>dashboard).dayInc;
-    }
+  getDayStat(): number {
+    return (<any>dashboard).dayStat;
+  }
+
+/* INCOME SUMMARY Balance */
+  getYearInc(): number {
+    return (<any>dashboard).yearInc;
+  }
+
+  getMonthInc(): number {
+    return (<any>dashboard).monthInc;
+  }
+
+  getWeekInc(): number {
+    return (<any>dashboard).weekInc;
+  }
+
+  getBiWeekInc(): number {
+    return (<any>dashboard).biweekInc;
+  }
+
+  getDayInc(): number {
+    return (<any>dashboard).dayInc;
+  }
+
+/* INCOME SUMMARY Collectables */
+
+  getYearCol(): number {
+    return (<any>dashboard).yearCol;
+  }
+
+  getMonthCol(): number {
+    return (<any>dashboard).monthCol;
+  }
+
+  getWeekCol(): number {
+    return (<any>dashboard).weekCol;
+  }
+
+  getBiWeekCol(): number {
+    return (<any>dashboard).biweekCol;
+  }
+
+  getDayCol(): number {
+    return (<any>dashboard).dayCol;
+  }
 
   constructor() { }
 
